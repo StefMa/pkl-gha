8376f2af40b4b6da8cf9b1bf1c5145543cb521ce
StefMa <StefMaDev@outlook.com>
2025-06-05 14:30:34 +0200

    Docs for pkl-gha

---
+++ data/com.github.action/0.0.5/Workflow/WindowsLatest.js
@@ -0,0 +1,1 @@
+runtimeData.links('known-versions','[{"text":"0.0.5","classes":"current-version"}]');
---
+++ data/com.github.action/0.0.5/actions/Setup/Java.js
@@ -0,0 +1,2 @@
+runtimeData.links('known-versions','[{"text":"0.0.5","classes":"current-version"}]');
+runtimeData.links('known-usages','[{"text":"Action","href":"../../Action/index.html"},{"text":"Setup","href":"index.html"}]');
